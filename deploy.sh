927de70dc839f362c950f9db9a19703d5f84db1d
Keiichiro Ui <keiichiro.ui@gmail.com>
2013-08-07 13:40:35 +0900

    add a simple deploy script

---
+++ deploy.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+
+DEST_HOSTS=
+ID_KEY=~/tmp/a.pem
+
+if [ -z "$DEST_HOSTS" ]
+then
+    echo DEST_HOSTS を設定してください
+    echo 例: DEST_HOSTS=\"user@foo.com user2@bar.com\"
+    exit 1
+fi
+
+if [ -z "$ID_KEY" ]
+then
+    echo ID_KEY を設定してください
+    echo 例: ID_KEY=~/.ssh/id_rsa
+    exit 1
+fi
+
+BASE_DIR=$(cd $(dirname $0); pwd)
+DEST_DIR=$(basename "${BASE_DIR}")
+
+set -eux
+
+for host in $DEST_HOSTS
+do
+    rsync -avz --exclude=".git" \
+        --rsh "ssh -i ${ID_KEY}" \
+        "${BASE_DIR}" \
+        "${host}:"
+
+    ssh -i "${ID_KEY}" "${host}" "cd '${DEST_DIR}' && pwd && bundle"
+done
